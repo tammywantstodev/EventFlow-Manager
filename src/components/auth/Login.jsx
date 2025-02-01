ea5e98db42473f02acc90cc43b96c2384f260ce8
Naftaly Ngeru <n.ngeru.k@gmail.com>
2025-02-01 23:53:30 +0300

    finalized authentication

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 //login component
 function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const proceedLogin = async (e) => {
+        e.preventDefault();
+
+
+        const response = await fetch("http://localhost:8000/users");
+            if (!response.ok) {
+                throw new Error("Failed to fetch users");
+            }
+
+            const users = await response.json();
+            const user = users.find(user => user.email === email);
+
+            if (user) {
+                if (user.password === password) {
+                    alert("Log In Successful");
+                    navigate('/home');
+                } else {
+                    alert("Wrong Password");
+                }
+            } else {
+                alert("Email not Found. Try Signing Up");
+            }
+    }
+
     return(
         <div className='login'>
             <h4>Login to Event Manager</h4>
-            <form>
+            <form onSubmit={proceedLogin}> 
                 <div>
                     <label htmlFor='email'>Email: </label>
-                    <input type='email' id='email' name='email' required />
+                    <input value={email} onChange={e => setEmail(e.target.value)} type='email' id='email' name='email' required />
                 </div>
                 <div>
                     <label htmlFor='password'>Password: </label>
-                    <input type='password' id='password' name='password' required />
+                    <input value={password} onChange={e => setPassword(e.target.value)} type='password' id='password' name='password' required />
                 </div>
                 <button type='submit'>Login</button>
             </form>
